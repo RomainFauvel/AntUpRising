0991b91b8e109564e4d966771aa1189fc2fb7685
mpek29 <florianpasco@protonmail.com>
2023-02-04 18:21:14 +0100

    Basic animation of the character

--- Assets/Florian/PlayerController.cs
+++ Assets/Florian/PlayerController.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+
+    public float xScale;
 
     private RaycastHit2D hit;
 
@@ -12,6 +15,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        xScale = Math.Abs(transform.localScale.x);
     }
 
     private void Update(){
@@ -20,6 +25,7 @@ public class PlayerController : MonoBehaviour
         //inverser le gravity scale
         if (Input.GetButtonDown("Jump")){
             GetComponent<Rigidbody2D>().gravityScale = -GetComponent<Rigidbody2D>().gravityScale;
+            transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
         }
 
         // Get the position of your player
@@ -33,5 +39,13 @@ public class PlayerController : MonoBehaviour
 
         Debug.DrawRay(transform.position, -transform.up * 2, Color.red);
         rigidbody2d.velocity = moveInput;
+
+        print(Input.GetAxisRaw("Horizontal"));
+
+        if(Input.GetAxisRaw("Horizontal") == 1){
+            transform.localScale = new Vector3(-xScale, transform.localScale.y, transform.localScale.z);
+        }else if(Input.GetAxisRaw("Horizontal") == -1){
+            transform.localScale = new Vector3(xScale, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
